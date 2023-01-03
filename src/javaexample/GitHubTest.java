3093ed63c96e50b3da455761bc3073d8dd6e6346
Fatih Deniz <deniz.fatih@hotmail.de>
2023-01-03 18:14:59 +0100

    Second commit

--- src/javaexample/GitHubTest.java
+++ src/javaexample/GitHubTest.java
@@ -3,5 +3,7 @@ package javaexample;
 public class GitHubTest {
     public static void main(String[] args) {
         System.out.println("Github Project");
+
+        System.out.println("This is a second commit");
     }
 }
